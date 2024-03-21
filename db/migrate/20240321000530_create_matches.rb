63a140e2cab03e51bf0ba54415e38d5e6539f3f1
artufin <arturo.herreros@uc.cl>
2024-03-21 18:43:12 -0300

    migracion

--- db/migrate/20240321000530_create_matches.rb
+++ db/migrate/20240321000530_create_matches.rb
@@ -1,8 +1,8 @@
 class CreateMatches < ActiveRecord::Migration[7.0]
   def change
     create_table :matches do |t|
-      t.references :teamA, null: false, foreign_key: true
-      t.references :teamB, null: false, foreign_key: true
+      t.references :teamA, null: false, foreign_key: { to_table: :teams }
+      t.references :teamB, null: false, foreign_key: { to_table: :teams }
       t.boolean :state
       t.string :result
 
